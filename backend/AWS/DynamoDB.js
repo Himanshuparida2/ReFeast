426b6fa53bb0de1ef82481c46a295abc2e45b2cb
Himanshu Sekhar Parida <himanshu.parida16@gmail.com>
2025-02-14 11:44:17 +0530

    backend

--- backend/AWS/DynamoDB.js
+++ backend/AWS/DynamoDB.js
@@ -1,8 +1,9 @@
 const AWS=require('aws-sdk');
+require('dotenv').config()
 AWS.config.update({
     region:'us-east-1',
-    accessKeyId:'AKIA6ODU2S7O27XWWE6A',
-    secretAccessKey:'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'
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
 })
 const dynamodb=new AWS.DynamoDB.DocumentClient();
 module.exports=dynamodb;
